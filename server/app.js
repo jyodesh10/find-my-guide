9e92e5788bbe7ff6539613001bf207aa2b63031d
jyodesh10 <jyodeshshakya@gmail.com>
2025-01-24 00:39:26 +0545

    adminjs uncommented

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
-// import AdminJSExpress from "@adminjs/express";
-// import * as AdminJSMongoose from "@adminjs/mongoose";
-// import AdminJS from "adminjs";
-// import MongoStore from "connect-mongo";
+import AdminJSExpress from "@adminjs/express";
+import * as AdminJSMongoose from "@adminjs/mongoose";
+import AdminJS from "adminjs";
+import MongoStore from "connect-mongo";
 import dotenv from "dotenv";
 import express from "express";
 import dbconnection from "./database.js";
-// import blogResource from "./resources/blog.resource.js";
-// import bookingResource from "./resources/booking.resource.js";
-// import guideResource from "./resources/guide.resource.js";
-// import homeResource from "./resources/home.resource.js";
-// import reviewResource from "./resources/review.resource.js";
-// import tourResource from "./resources/tour.resource.js";
-// import tourreviewResource from "./resources/tour_review.resource.js";
-// import userResource from "./resources/user.resource.js";
+import blogResource from "./resources/blog.resource.js";
+import bookingResource from "./resources/booking.resource.js";
+import guideResource from "./resources/guide.resource.js";
+import homeResource from "./resources/home.resource.js";
+import reviewResource from "./resources/review.resource.js";
+import tourResource from "./resources/tour.resource.js";
+import tourreviewResource from "./resources/tour_review.resource.js";
+import userResource from "./resources/user.resource.js";
 import authRoute from "./routes/auth.route.js";
 import blogRoute from "./routes/blog.route.js";
 import bookingRoute from "./routes/booking.route.js";
@@ -26,67 +26,67 @@ import tourreviewRoute from "./routes/tour_review.route.js";
 import userRoute from "./routes/users.route.js";
 import wishlistRoute from "./routes/wishlist.route.js";
 
-// AdminJS.registerAdapter(AdminJSMongoose);
+AdminJS.registerAdapter(AdminJSMongoose);
 dotenv.config();
 const app = express();
 
 
-// //admins
-// const adminJs = new AdminJS({
-//     resources: [
-//         // homeResource
-//         userResource,
-//         blogResource,
-//         tourResource,
-//         guideResource,
-//         tourreviewResource,
-//         homeResource,
-//         reviewResource,
-//         bookingResource
-//     ],
-//     rootPath: "/admin", // Path to the AdminJS dashboard.
-// });
+//admins
+const adminJs = new AdminJS({
+    resources: [
+        // homeResource
+        userResource,
+        blogResource,
+        tourResource,
+        guideResource,
+        tourreviewResource,
+        homeResource,
+        reviewResource,
+        bookingResource
+    ],
+    rootPath: "/admin", // Path to the AdminJS dashboard.
+});
 
-// // const ConnectSession = MongoStore(session);
-// const sessionStore = MongoStore.create({
-//     mongoUrl: process.env.URI, // Replace with your connection string
-//     collection: 'sessions',
-// })
+// const ConnectSession = MongoStore(session);
+const sessionStore = MongoStore.create({
+    mongoUrl: process.env.URI, // Replace with your connection string
+    collection: 'sessions',
+})
 
-// const DEFAULT_ADMIN = {
-//     email: process.env.ADMIN_EMAIL,
-//     password: process.env.ADMIN_PASSWORD,
-// }
+const DEFAULT_ADMIN = {
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+}
 
-// const authenticate = async (email, password) => {
-//     if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
-//         return Promise.resolve(DEFAULT_ADMIN)
-//     }
-//     return null
-// }
+const authenticate = async (email, password) => {
+    if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+        return Promise.resolve(DEFAULT_ADMIN)
+    }
+    return null
+}
 
-// const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
-//     adminJs,
-//     {
-//         authenticate,
-//         cookieName: 'adminjs',
-//         cookiePassword: 'sessionsecret',
-//     },
-//     null,
-//     {
-//         store: sessionStore,
-//         resave: true,
-//         saveUninitialized: true,
-//         secret: 'sessionsecret',
-//         // cookie: {
-//         //     httpOnly: process.env.NODE_ENV === 'production',
-//         //     secure: process.env.NODE_ENV === 'production',
-//         // },
-//         name: 'adminjs',
-//     }
-// )
+const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+    adminJs,
+    {
+        authenticate,
+        cookieName: 'adminjs',
+        cookiePassword: 'sessionsecret',
+    },
+    null,
+    {
+        store: sessionStore,
+        resave: true,
+        saveUninitialized: true,
+        secret: 'sessionsecret',
+        // cookie: {
+        //     httpOnly: process.env.NODE_ENV === 'production',
+        //     secure: process.env.NODE_ENV === 'production',
+        // },
+        name: 'adminjs',
+    }
+)
 
-// app.use(adminJs.options.rootPath, adminRouter);
+app.use(adminJs.options.rootPath, adminRouter);
 
 //middleware
 app.use(express.json());
