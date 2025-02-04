aac69bfa0a6a8cc135450757e6a20c29ed3a8383
jyodesh10 <jyodeshshakya@gmail.com>
2025-02-04 19:19:26 +0545

    added cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 // import * as AdminJSMongoose from "@adminjs/mongoose";
 // import AdminJS from "adminjs";
 // import MongoStore from "connect-mongo";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import dbconnection from "./database.js";
@@ -90,6 +91,7 @@ const app = express();
 // app.use(adminJs.options.rootPath, adminRouter);
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/uploads', express.static('uploads'));
@@ -116,7 +118,7 @@ app.use("/auth", authRoute);
 
 const startserver = () => {
     dbconnection().then(() => {
-        const PORT = 3000;
+        const PORT = 3030;
         app.listen(PORT, () => {
             console.log(`Server running at localhost:${PORT}`);
         });
