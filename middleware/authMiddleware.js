274283e8a3d6c719f50f97a6801aefd9475e934d
jyodesh10 <jyodeshshakya@gmail.com>
2025-06-22 00:11:42 +0200

    tour delete route

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,10 +5,16 @@ const authenticateToken = (req, res, next) => {
     if (!token)
         return res.status(401).json({ message: "access denied" });
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err)
-            return res.status(403).json({ message: "invalid token" });
-        req.user = user.user._id;
-        next();
+        try {
+            if (err)
+                return res.status(403).json({ message: "invalid token" });
+            console.log("JWT Payload:", user);
+            req.user = user.user._id;
+            next();
+        } catch (error) {
+            console.error("Error in JWT verify callback:", error);
+            res.status(500).json({ message: "Internal server error" });
+        }
     });
 };
 export default authenticateToken;
--- server/routes/tour.route.js
+++ server/routes/tour.route.js
@@ -1,10 +1,10 @@
 import * as express from "express";
 import authenticateToken from "../../middleware/authMiddleware.js";
-import { createTour, getAllTours, getTour, deleteTour } from "../controllers/tour.controller.js";
 import upload from "../../middleware/uploadMiddleware.js";
+import { createTour, deleteTour, getAllTours, getTour } from "../controllers/tour.controller.js";
 const router = express.Router();
 router.post("/", [authenticateToken, upload.single('image')], createTour);
 router.get("/", authenticateToken, getAllTours);
 router.get("/:id", authenticateToken, getTour);
-router.put("/:id", authenticateToken, deleteTour);
+router.delete("/:id", authenticateToken, deleteTour);
 export default router;
